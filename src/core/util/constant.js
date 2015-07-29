6ad5e8c0d65db77789b70ee33e387a99482333a8
SpeedoDevo <speedo@devo.hu>
2015-07-29 15:30:47 +0100

    xs, lt

--- src/core/util/constant.js
+++ src/core/util/constant.js
@@ -39,8 +39,12 @@ function MdConstantFactory($sniffer) {
     MEDIA: {
       'xs': '(max-width: 399px)',
       'gt-xs': '(min-width: 400px)',
+<<<<<<< HEAD
       'lt-sm': '(max-width: 599px)',
       'sm': '(min-width: 400px) and (max-width: 599px)',
+=======
+      'sm': '(max-width: 599px)',
+>>>>>>> xs, lt
       'gt-sm': '(min-width: 600px)',
       'lt-md': '(max-width: 959px)',
       'md': '(min-width: 600px) and (max-width: 959px)',
@@ -60,6 +64,7 @@ function MdConstantFactory($sniffer) {
       'sm',
       'lt-sm',
       'gt-xs',
-      'xs',    ]
+      'xs'
+    ]
   };
 }
